cf2efdb4b48c3e8be69e8337e8eb566a66d4a0d8
mjtedder <mjtedder@gmail.com>
2019-03-12 12:49:58 -0600

    Add additional if statement
    I added another statement which checks the parameters for a prop that
    equals tracks but the id for the album passed in does NOT have a tracks
    property, the code block executed will create a tracks array and then
    push the value argument into the array.  This has been tested and works.

--- JS Algorithims And Data Structures/Basic JavaScript/record.js
+++ JS Algorithims And Data Structures/Basic JavaScript/record.js
@@ -1,46 +1,49 @@
 var collection = {
-    "2548": {
+  "2548": {
       "album": "Slippery When Wet",
       "artist": "Bon Jovi",
       "tracks": [
-        "Let It Rock",
-        "You Give Love a Bad Name"
+          "Let It Rock",
+          "You Give Love a Bad Name"
       ]
-    },
-    "2468": {
+  },
+  "2468": {
       "album": "1999",
       "artist": "Prince",
       "tracks": [
-        "1999",
-        "Little Red Corvette"
+          "1999",
+          "Little Red Corvette"
       ]
-    },
-    "1245": {
+  },
+  "1245": {
       "artist": "Robert Palmer",
-      "tracks": [ ]
-    },
-    "5439": {
+      "tracks": []
+  },
+  "5439": {
       "album": "ABBA Gold"
-    }
+  }
 };
 // Keep a copy of the collection for tests
 var collectionCopy = JSON.parse(JSON.stringify(collection));
 
 // Only change code below this line
 function updateRecords(id, prop, value) {
-    // If prop isn't tracks and value isn't empty
-    if (prop !== "tracks" && value !== null) {
-    // Update or set the value for that record album's property
-    collection[id][prop] = value
-    console.log(collection)
+  // If prop isn't tracks and value isn't empty
+  if (prop !== "tracks" && value !== null) {
+      // Update or set the value for that record album's property
+      collection[id][prop] = value
+      console.log(collection)
   }
-    // If prop is "tracks" but the album doesn't have a "tracks" property
-    else if (prop === "tracks" && "tracks" === null) {
-    var tracks = []
-    tracks.push(collection.id.prop.value)
-     }
+  // If prop is "tracks" but the album doesn't have a "tracks" property
+  else if (prop === "tracks" && collection[id]["tracks"] === undefined) {
+      // Create an empty array before adding the new value to the album's corresponding property
+      var tracks = []
+      tracks.push(value)
+      collection[id][prop] = tracks
+      console.log(collection)
+  }
   return collection;
 }
 
 // Alter values below to test your code
-updateRecords(5439, "artist", "ABBA");
+updateRecords(5439, "tracks", "Take A Chance On Me");
