6d46ba1a8492937c12bab8e33218c29e8d6f5db5
mjtedder <mjtedder@gmail.com>
2019-03-14 14:18:14 -0600

    Function sucessfully returns requested data
    The lookUpProfile function takes in two parameters (name, prop).  Then
    it checks the object to make sure the name exists in contacts.  If it
    does it checks the prop argument and prints those to the console.  If
    the name is not found, it logs "No such contact" to the console.
    
    TODO: Find a way to not print No such contact when a contact DOES
    exist.  Right now, the function works, but always prints 'No such
    contact' even when the desired contact is present.

--- JS Algorithims And Data Structures/Basic JavaScript/profileLookup.js
+++ JS Algorithims And Data Structures/Basic JavaScript/profileLookup.js
@@ -28,22 +28,21 @@ var contacts = [{
 // The function should check:
 function lookUpProfile(name, prop) {
     // If name is an actual contact's firstName and the given property (prop) is a property of that contact.
-        for (i = 0; i < contacts.length; i++) {
-            if (name === contacts[i].firstName) {
-                console.log('name exists!')
-                if (contacts[i].hasOwnProperty(prop)) {
-                    // If both are true, then return the "value" of that property.
-                    console.log(contacts[i][prop])
-                } else {
-                    console.log('error')
-                }
+    for (i = 0; i < contacts.length; i++) {
+        if (name === contacts[i].firstName) {
+            // console.log('name exists!')
+            if (contacts[i].hasOwnProperty(prop)) {
+                // If both are true, then return the "value" of that property.
+                console.log(contacts[i][prop])
+                // If prop does not correspond to any valid properties of a contact found to match name then return "No such property"
+            } else {
+                console.log('No such property')
             }
         }
     }
-
-// If name does not correspond to any contacts then return "No such contact"
-
-// If prop does not correspond to any valid properties of a contact found to match name then return "No such property"
+    // If name does not correspond to any contacts then return "No such contact"
+    console.log('No such contact')
+}
 
 // Change these values to test your function
-lookUpProfile("Harry", "likes");
+lookUpProfile("Akira", "likes");
