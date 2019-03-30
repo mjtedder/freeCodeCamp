a3b6dd887388a7f4446414bd9d4d2a595d5a7b46
mjtedder <mjtedder@gmail.com>
2019-03-30 08:02:06 -0600

    Use multiple conditional statements
    To evaluate the user's strokes against the par of the hole, the numbers
    are passed into a function called golfScore.  Multiple conditional
    statments will be used to determine what index in the names array to
    print to the console.
    
    The names array holds multiple strings each one represent a score a
    player can attain in a hole of golf.

--- JS Algorithims And Data Structures/Basic JavaScript/golf.js
+++ JS Algorithims And Data Structures/Basic JavaScript/golf.js
@@ -1,12 +1,13 @@
 var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
 
 function golfScore(par, strokes) {
-    // Only change code below this line
-
-
-    return "Change Me";
-    // Only change code above this line
+    if (strokes === 1) {
+        console.log(names[0])
+    } else if (strokes <= par - 2) {
+        console.log(names[1])
+    }
 }
 
+
 // Change these values to test
-golfScore(5, 4);
+golfScore(5, 3);
